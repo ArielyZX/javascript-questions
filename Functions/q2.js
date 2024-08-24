f697ba4017176257fa899feb1bc3d449742ff0b6
arielyzx <arielyoliveira1205@gmail.com>
2024-08-24 14:44:28 -0300

    obs: está dando erro no prompt.

--- Functions/q2.js
+++ Functions/q2.js
@@ -1,8 +1,8 @@
 function par() {
+    const num = prompt("Digite um número: ")
     if (num % 2 === 0) {
         return "O número escolhido é par.";
     } else {
         return "O número escolhido é ímpar.";
     }
-}
-console.log(parimpar(17));
+} par()
