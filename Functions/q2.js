8eff3640cb621a59fca12f9b57b5b800874b84b2
arielyzx <arielyoliveira1205@gmail.com>
2024-08-24 14:05:25 -0300

    alterando a questão 2.

--- Functions/q2.js
+++ Functions/q2.js
@@ -1,8 +1,8 @@
-function parimpar(num){
-   if(num % 2 === 0){
-       return 'true';
-   }else{
-       return 'false';
-   }
+function par() {
+    if (num % 2 === 0) {
+        return "O número escolhido é par.";
+    } else {
+        return "O número escolhido é ímpar.";
+    }
 }
 console.log(parimpar(17));
